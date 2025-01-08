8a0cdee29e5bafcba41383cfba30c787394905a0
AmirMohammad <155167947+Amirmoradi8@users.noreply.github.com>
2025-01-09 00:31:25 +0330

    Update main.py

--- core/main.py
+++ core/main.py
@@ -1,12 +1,11 @@
 import telebot
+import os
 from googletrans import Translator
 
 API_TOKEN = '7402774915:AAE7h-yNvMxxHKE8UfCf16slajYpQ48YYfw'
 
 bot = telebot.TeleBot(API_TOKEN)
-
 translate = Translator()
-
 
 @bot.message_handler(commands=['start'])
 def start_Bot(message):
@@ -16,10 +15,13 @@ def start_Bot(message):
 def help_Bot(message):
     bot.reply_to(message , 'Hi\nhow can i help you?')
 
-
 @bot.message_handler(func=lambda message:True)
-def wellcome_mesage(message):
-    translated_text = translate.translate(message.text , src='en' , dest='fa')
-    bot.reply_to(message , translated_text.text)
+def translate_command(message):
+    text = message.text
+    language = text.split()[-1]
+    translate = Translator()
+    translated_text= translate.translate(text , dest=language).text
+    bot.reply_to(message, translated_text)
 
+# bot.infinity_polling()
 bot.infinity_polling()
