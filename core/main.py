4a68d8bd09874a4609f15b9304cfa93eea1c1c88
AmirMohammad <155167947+Amirmoradi8@users.noreply.github.com>
2025-01-08 21:53:08 +0330

    telegram-bot

--- core/main.py
+++ core/main.py
@@ -5,9 +5,17 @@ API_TOKEN = '7402774915:AAE7h-yNvMxxHKE8UfCf16slajYpQ48YYfw'
 bot = telebot.TeleBot(API_TOKEN)
 
 
+@bot.message_handler(commands=['start'])
+def start_Bot(message):
+    bot.reply_to(message , 'Hi\nhow can i help you?')
+
+@bot.message_handler(commands=['help'])
+def help_Bot(message):
+    bot.reply_to(message , 'Hi\nhow can i help you?')
 
 
-
-
+@bot.message_handler(func=lambda message:True)
+def wellcome_mesage(message):
+    bot.reply_to(message , 'salammmmmmm')
 
 bot.infinity_polling()
